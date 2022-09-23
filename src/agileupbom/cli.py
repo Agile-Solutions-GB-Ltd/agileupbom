982fd354c7adbf617e3d6f7fed6232b1270845fd
devops <devops@localhost.localdomain>
2022-09-23 10:04:30 +0100

    Update project

--- src/agileupbom/cli.py
+++ src/agileupbom/cli.py
@@ -4,14 +4,14 @@ import pkg_resources
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.group(help='Pipeline support cli.')
+@click.group(help='CLI to support pipeline bill-of-materials management.')
 def cli() -> int:
     pass
 
 
 @cli.command(help='Display the current version.')
 def version() -> None:
-    click.echo(f"Version: {pkg_resources.get_distribution('agileup').version}")
+    click.echo(f"AgileUP BOM Version: {pkg_resources.get_distribution('agileupbom').version}")
 
 
 @cli.command(help='Check client configuration.')
